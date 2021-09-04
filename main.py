e8606ec6739763ea554acfd3d52e50bc3fe44e60
kdhawan2000 <77895143+kdhawan2000@users.noreply.github.com>
2021-09-04 11:55:48 -0600

    Update main.py
    Added a few new commands like ?get_quote  that use an api to respond with a random quote when triggered

--- main.py
+++ main.py
@@ -6,68 +6,136 @@ from keep_alive import keep_alive
 import requests
 import json
 
+intents = discord.Intents.all()
+client = commands.Bot(command_prefix='?', intents=intents)
 
-client = commands.Bot(command_prefix='?')
-
-
+#this simply prints when the bot is online on the console
 @client.event
 async def on_ready():
-  print('We are online now with the bot account called {0.user}'.format(client))
+    print('We are online now with {0.user}'.format(client))
 
+
+#this sends a direct message to the person joining a server containing this bot
 @client.event
-async def on_member_join(member):
-    print(f'{member} has joined the server!')
+async def on_member_join(member: discord.Member):
+  channel = await member.create_dm()
+  await channel.send(f'Hi {member.name}, welcome to my Discord server!')
 
+# @client.event
+# async def on_member_remove(ctx, member):
+#    await ctx.send(f'{member} has left the server!')
 
-@client.event
-async def on_member_remove(member):
-    print(f'{member} has left the server!')
-
-@client.command()
-async def commands(ctx):
-  await ctx.send ("We got 7 commands right now. This includes ?commands, ?dice, ?hello, ?hi, ?kick, ?botstatus and ?clear. ")
-
-@client.command()
-async def botstatus(ctx):
-  await ctx.send ("This bot shouldnt get offline if Fusion turns off his laptop too.")
-
+# clear <number> lets the user delete messages easily
 @client.command()
 async def clear(ctx, amount=3):
     await ctx.channel.purge(limit=amount)
 
+#this makes the bot respond with a random quote when user uses the command ?get_quote using an api
+@client.command()
+async def get_quote(ctx):
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + " -" + json_data[0]['a']
+    await ctx.send(quote)
+
+# this makes the bot respond with the quote of the day when user types in ?qotd using an api
+@client.command()
+async def qotd(ctx):
+    response = requests.get("https://zenquotes.io/api/today")
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + " - " + json_data[0]['a']
+    await ctx.send(quote)
+
+# this command makes the bot reply with a random quote from the show The Office by using an api
+@client.command()
+async def office_quote(ctx):
+    response = requests.get("https://officeapi.dev/api/quotes/random")
+    json_data = json.loads(response.text)
+    quotes = json_data['data']['content'] + " - " + json_data["data"]['character']['firstname'] + " " + json_data["data"]['character']['lastname']
+    await ctx.send(quotes)
+
+# this command makes the bot reply with a random quote from the show Brooklyn99 by using an api
+@client.command()
+async def b99_quote(ctx):
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        'DADDYYY',
+        'Cool cool cool cool cool cool cool cool, no doubt no doubt no doubt no doubt.'
+    ]
+    
+    await ctx.send(random.choice(brooklyn_99_quotes))
+
+# One of the first commands made for this bot, this command lets the user know that the bot online and working.
+@client.command()
+async def botstatus(ctx):
+    await ctx.send(
+        "If you recieved a response the bot is running just fine.")
+
+# this commands rolls a dice 
 @client.command()
 async def dice(ctx):
-  variable = {
-    'one',
-    'two',
-    'three',
-    'four',
-    'five',
-    'six'
-  }
-  await ctx.send({random.choice(variable)})
+    variable = ['one', 'two', 'three', 'four', 'five', 'six']
+    await ctx.send(random.choice(variable))
 
+# this command gives a random reply when asked a question
+@client.command(aliases=['8ball'])
+async def _8ball(ctx, *, question):
+    responses = [
+        'It is certain',
+        'It is decidedly so', 
+        'Without a doubt',
+        'Yes, definitely',
+        'You may rely on it', 
+        'As I see it, yes',
+        'Most likely', 
+        'Outlook good', 
+        'Yes', 
+        'Signs point to yes'
+        'Reply hazy try again', 
+        'Ask again later', 
+        'Better not tell you now',
+        'Cannot predict now', 
+        'Concentrate and ask again', 
+        "Don't count on it",
+        'My reply is no', 
+        'My sources say no', 
+        'Outlook not so good',
+        'Very doubtful'
+    ]
+    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+
+# this command lets the user kick anyone in the server except the owner of the server with reasoning
 @client.command()
 async def kick(ctx, member: discord.Member, *, reason=None):
     await member.kick(reason=reason)
     await ctx.send(f'Kicked {member.mention} for {reason}')
 
+# this commands informs the user about the bot
 @client.command()
 async def hello(ctx):
-  await ctx.send ("bhak yaar kitni baar hello bologe.")
+    await ctx.send("Hello, thanks for using this bot. You can add this bot to your server using https://bit.ly/anodiscordbot . To learn more about the commands the bot supports right now, type in ?commands")
 
+# this command responds with a list of command the bot works with 
+@client.command()
+async def commands(ctx):
+    await ctx.send("We got 12 commands right now. The prefix to these commands are ?. The list of commands includes: ?hello, ?commands, ?hi, ?get_quote, ?qotd, ?office_quote, ?b99_quote, ?botstatus, ?8ball, ?dice, ?kick, and ?clear. ")
+
+#this bot responds with a random response to the user's hi
 @client.command()
 async def hi(ctx):
-  variable = {
-    'Hello noob',
-    'chutta nahi hai',
-    'mujhe bolna hi nahi hai im not interested yet',
-    'supppp',
-    'get a job instead of talking to a bot',
-    'hi sir'
-  }
-  await ctx.send({random.choice(variable)})
+    variable = [
+      'Hello sir', 
+      'WAASSSUP',
+      'Bonjour!', 
+      'supppp',
+      'Hey! Getting bored? Try out ?commands for the list  of commands you can try', 
+      'hi sir'
+     ]    
+    await ctx.send(random.choice(variable))
 
+    
 keep_alive()
-token = os.environ['Raaz']
+token = os.environ['Secret']
 client.run(token)
